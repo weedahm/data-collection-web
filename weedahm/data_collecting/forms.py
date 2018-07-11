bccb6ef1b17fc28cc8d489e4837ac94f01d7f732
chacha <jjcec@naver.com>
2018-07-11 18:31:24 +0900

    20180711 수정사항

---
+++ weedahm/data_collecting/forms.py
@@ -0,0 +1,8 @@
+# from django import forms
+# from .models import 무엇
+
+# class PostForm(forms.ModelForm):
+
+#     class Meta:
+#         model = 무엇
+#         fields = ('왓', '음',)
--- weedahm/data_collecting/views.py
+++ weedahm/data_collecting/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.template import loader
 from django.http import HttpResponse
+# from .forms import PostForm
 
 # Create your views here.
 
 def index(request):
     return render(request, 'data_collecting/main.html')
+
+# def post(request):
+#     if request.method == "POST":
+#         #save data
+#         form = PostForm(request.POST)
+#         if form.is_valid():
+#             lotto = form.save(commit = False)
+#             lotto.generate()
+#             return redirect('index')
+    
+#     else:
+#         form = PostForm()
+#         return render(request, 'data_collecting/form.html', {'form' : form})
