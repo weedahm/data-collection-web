51d586cb62136797df1f3bf0b4a51557a177b3d3
banana <2323hsb@gmail.com>
2018-07-13 18:55:44 +0900

    로그인 기능 추가

--- weedahm/data_collecting/urls.py
+++ weedahm/data_collecting/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 app_name = 'data_collecting'
 urlpatterns = [
+    path('login', views.login, name='login'),
+    path('authenticate', views.authenticate, name='authenticate'),
     path('', views.index, name='index'),
     path('submit', views.submit, name='submit'),
     path('patients/', views.patients, name='patients'),
--- weedahm/data_collecting/views.py
+++ weedahm/data_collecting/views.py
@@ -1,14 +1,34 @@
 from django.shortcuts import render
 from django.template import loader
-from django.http import HttpResponse
-from .models import Patient
+from django.http import HttpResponse, HttpResponseRedirect
 from django.http import JsonResponse
+from django.contrib.auth.models import User
+from django.contrib import auth
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+
+from .models import Patient
 
 import json
 
+def login(request):
+    return render(request, 'data_collecting/login.html')
+
+def authenticate(request):
+	username = request.POST['username']
+	password = request.POST['password']
+	user = auth.authenticate(request, username=username, password=password)
+	if user is not None:
+		auth.login(request, user)
+		return HttpResponseRedirect(reverse('data_collecting:index'))
+	else:
+		return HttpResponseRedirect(reverse('data_collecting:login'))
+
+@login_required(login_url='/login')
 def index(request):
     return render(request, 'data_collecting/main.html')
 
+@login_required(login_url='/login')
 def patient(request, chart_id):
     try:
         patient = Patient.objects.get(chart_id=chart_id)
@@ -16,6 +36,7 @@ def patient(request, chart_id):
         return JsonResponse({})
     return JsonResponse(patient.json_data)
 
+@login_required(login_url='/login')
 def patients(request):
     try:
         patient = Patient.objects.all().order_by("-chart_id")
@@ -24,6 +45,7 @@ def patients(request):
     chart_ids = list((patient.values('chart_id')))
     return JsonResponse(chart_ids, safe=False)
 
+@login_required(login_url='/login')
 def submit(request):
     received_json_data = json.loads(request.body)
     chart_id = received_json_data['chart_id']
